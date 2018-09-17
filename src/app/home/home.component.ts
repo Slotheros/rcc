e6c549b50bbbdb581bd12700f868aa1f1a58ce65
Rana Vemireddy <vemireddy.rana@gmail.com>
2018-09-17 19:09:43 -0400

    Temporary measures to demonstrate that the front-end can store files in the backend.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import {  FileUploader } from 'ng2-file-upload/ng2-file-upload';
+
+const URL = 'http://localhost:3000/users/csvCompare';
 
 @Component({
   selector: 'rcc-home',
@@ -9,8 +12,14 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  public uploader:FileUploader = new FileUploader({url: URL, itemAlias: 'csvCompare'});
+
   constructor(private authService: AuthService,
-              private router: Router) { }
+    private router: Router) {
+    this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any) => {
+      console.log("ImageUpload:uploaded:", item, status, response);
+    };
+  }
 
   ngOnInit() {
     this.authService.loggedIn().subscribe(result => {
