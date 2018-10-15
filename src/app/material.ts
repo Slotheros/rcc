a324b28b29a798d1e4f96ab344d6f33041b826d0
Louis Trapani <lxt2528@rit.edu>
2018-10-15 17:46:42 -0400

    added tooltip hover to policy url links. policy links open in new tab. added margin to bottom of container.

--- src/app/material.ts
+++ src/app/material.ts
@@ -4,18 +4,18 @@ import {MatIconModule, MatIcon} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatFormFieldModule, MatOptionModule, MatSelectModule, MatButtonModule, MatCheckboxModule, MatInputModule, MatSidenavModule,
-  MatCardModule, MatExpansionModule, MatSnackBarModule, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material';
+  MatCardModule, MatExpansionModule, MatSnackBarModule, MatDialogModule, MAT_DIALOG_DATA, MatTooltipModule } from '@angular/material';
 
 @NgModule({
     imports:
         [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule,
         MatMenuModule, MatFormFieldModule, MatOptionModule, MatSelectModule,
         MatInputModule, MatSidenavModule, MatCardModule, MatExpansionModule,
-        MatSnackBarModule, MatDialogModule, MatDividerModule],
+        MatSnackBarModule, MatDialogModule, MatDividerModule, MatTooltipModule],
     exports:
         [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule,
         MatMenuModule, MatFormFieldModule, MatOptionModule, MatSelectModule,
         MatInputModule, MatSidenavModule, MatCardModule, MatExpansionModule,
-        MatSnackBarModule, MatDialogModule, MatDividerModule],
+        MatSnackBarModule, MatDialogModule, MatDividerModule, MatTooltipModule],
 })
 export class MaterialModule { }
